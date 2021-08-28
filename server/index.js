141e04b0b3f6aa69d6b70f65bc94c9843f1e1d67
Warren Wong <warren.wong4@gmail.com>
2021-08-28 15:35:01 -0400

    Setup express server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./routes/index.js');
+const port = process.env.PORT || 3000;
+const morgan = require('morgan');
 
+app.use(express.static(__dirname + '/../dist/'));
+app.use(morgan('dev'));
 app.use('/', router);
+
+app.listen(port, () => {
+  console.log('server is running');
+});
