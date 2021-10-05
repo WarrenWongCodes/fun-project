a5a5bf24feae8966c139f7b7384bb015e72a9d14
Warren Wong <warren.wong4@gmail.com>
2021-10-05 10:02:46 -0400

    Update TS Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/],
+      },
     ],
   },
   mode: 'development',
